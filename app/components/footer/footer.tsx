f9ff408abff874b61582c8ddd712ff6feec8f25c
MuhammadOwais1412 <muhammadowais3782@gmail.com>
2024-10-14 15:41:32 +0500

    Pushing a Next.js Assignment

---
+++ app/components/footer/footer.tsx
@@ -0,0 +1,8 @@
+import style from "./footer.module.css"
+const Footer = () => {
+    return(<footer className={style.footer}>
+        <p>&copy;2024 All Rights Reserved</p>
+    </footer>)
+}
+
+export default Footer
